171dfa0b67f00cc3de865baaa3174b8fb3c015fe
karleighponnock <karleighponnock@gmail.com>
2020-08-27 18:11:25 -0400

    Rendering hello on page from index.js destination container

--- todolist/src/index.js
+++ todolist/src/index.js
@@ -1,5 +1,12 @@
-import react from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-var destination 
+var destination = document.querySelector("#container");
+
+ReactDOM.render(
+    <div>
+<p>Hello!</p>
+    </div>,
+    destination
+);
