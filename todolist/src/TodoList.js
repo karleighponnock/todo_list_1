e16c36382f34f4c0dfc6c24d6b9713ccc3e4bb7c
karleighponnock <karleighponnock@gmail.com>
2020-08-27 18:28:02 -0400

    item binding

--- todolist/src/TodoList.js
+++ todolist/src/TodoList.js
@@ -1,6 +1,18 @@
 import React, { Component } from "react";
 
 class TodoList extends Component {
+    constructor(props){
+        super(props)
+;
+
+this.addItem = this.addItem.bind(this);
+}
+
+addItem(e){
+
+}
+
+
     render() {
         return (
             <div className="todoListMain">
