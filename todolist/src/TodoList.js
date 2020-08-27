30df703ab0a2f29b14f6878eef7a2a81632d312b
karleighponnock <karleighponnock@gmail.com>
2020-08-27 18:31:55 -0400

    access content of input using _inputElement

--- todolist/src/TodoList.js
+++ todolist/src/TodoList.js
@@ -21,7 +21,8 @@ addItem(e){
             <div className="todoListMain">
                 <div className="header">
                     <form onSubmit={this.addItem}>
-                        <input placeholder="enter task">
+                        <input ref={(a) => this._inputElement = a}
+                            placeholder="enter task">
                         </input>
                         <button type="submit">add</button>
                     </form>
