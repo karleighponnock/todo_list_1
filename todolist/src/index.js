bcf50f718f044fa0a35f2460a33ba6eb6f2cd0c7
karleighponnock <karleighponnock@gmail.com>
2020-08-27 18:18:37 -0400

    import TodoList into index.js and render that file instead off the Hello

--- todolist/src/index.js
+++ todolist/src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import TodoList from "./TodoList"
 
 var destination = document.querySelector("#container");
 
 ReactDOM.render(
     <div>
-<p>Hello!</p>
+<p>
+<TodoList/>
+</p>
     </div>,
     destination
 );
