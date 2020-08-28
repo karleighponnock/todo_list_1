33effac80c1692b2aafb692b7be66c15eb6faa48
karleighponnock <karleighponnock@gmail.com>
2020-08-27 23:25:39 -0400

    set state todolist

--- todolist/src/TodoList.js
+++ todolist/src/TodoList.js
@@ -35,6 +35,17 @@ class TodoList extends Component {
         e.preventDefault();
     }
 
+    deleteItem(key) {
+        var filteredItems = this.state.items.filter(function (item) {
+            return(item.key !== key)
+        });
+
+
+        this.setState({
+            items: filteredItems
+        });
+    }
+
 
     render() {
         return (
@@ -48,7 +59,7 @@ class TodoList extends Component {
                     </form>
                 </div>
                 <TodoItems entries={this.state.items}/>
-                            delete={this.deleteItem}/>
+                            delete={this.deleteItem}/> 
             </div>
         );
     }
